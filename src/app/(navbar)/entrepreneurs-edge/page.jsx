6740a4888804b456ec5d82a4a3014f4a128075ee
Yash Shinde <technicals@vrt9.net>
2025-11-09 12:26:56 +0530

    Updated link of ee page

--- src/app/(navbar)/entrepreneurs-edge/page.jsx
+++ src/app/(navbar)/entrepreneurs-edge/page.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <div>
+        <h1 className='text-4xl font-bold'>Entrepreneurs EDGE™</h1>
+        <p className='text-lg'>Entrepreneurs EDGE™ is a program that helps entrepreneurs grow their businesses.</p>
+        <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Get Started</button>
+        <button className='bg-red-500 text-white px-4 py-2 rounded-md'>Get Started</button>
+      </div>
+      <div>
+        <h2 className='text-2xl font-bold'>What is Entrepreneurs EDGE™?</h2>
+      </div>
+    </div>
   )
 }
 
--- src/components/home/navigation-header.jsx
+++ src/components/home/navigation-header.jsx
@@ -38,7 +38,7 @@ const navItems = [
         subItems: [
             { label: "EGA™", href: "/ega" },
             { label: "EGOS™", href: "/ego" },
-            { label: "Entrepreneur’s EDGE™", href: "/entrepreneurs-edge" },
+            { label: "Entrepreneur’s EDGE™", href: "https://brief.egavrt.com" },
             { label: "Business Stages of Growth", href: "/sog" },
         ],
     },
